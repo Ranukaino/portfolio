948e028dfe17fb27bb8a927c1458cb8e060ec00b
Ranukaino <nombre4737362@gmail.com>
2025-06-14 21:29:16 -0300

    implementação de componente botão

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from "./components/button/button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'portfolio';
+  public button1 = "kkk";
+  public button2 = "kkk2";
+
+  public acao1 = ()=>{
+    window.alert("button1 foi clicado")
+  }
+
+  public acao2 = ()=>{
+    window.alert("button2 foi clicado")
+  }
 }
---
+++ src/app/components/button/button.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss'
+})
+export class Button {
+  @Input() name: string | undefined;
+  @Input() acao: void | undefined;
+}
